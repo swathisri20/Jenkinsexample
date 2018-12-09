7c0e165d632bd277cee8cd32fa14963071fed8c7
dckap <swathisrir@dckap.com>
2018-12-09 11:49:53 +0530

    2nd commit

--- example.java
+++ example.java
@@ -2,6 +2,7 @@ public class example{
 	public static void main(String[] args){
 		System.out.println("------------------------------------------");
 		System.out.println("QA touch");
+		System.out.println("Smarter Test Management For QA Teams");
 		System.out.println("------------------------------------------");
 	}
 }
