c1c8e607e3974449ce452cd741c32091955e6da9
dckap <swathisrir@dckap.com>
2018-12-09 11:20:04 +0530

    rename

---
+++ example.java
@@ -0,0 +1,7 @@
+public class helloworld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("Hi World");
+		System.out.println("Good evening");
+	}
+}
