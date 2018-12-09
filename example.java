4fd8769cfb6be2e18bb076b92c9f7ed5550ec47d
dckap <swathisrir@dckap.com>
2018-12-09 11:21:34 +0530

    change

--- example.java
+++ example.java
@@ -1,7 +1,7 @@
 public class helloworld{
 	public static void main(String[] args){
-		System.out.println("Hello World");
-		System.out.println("Hi World");
-		System.out.println("Good evening");
+		System.out.println("------------------------------------------");
+		System.out.println("QA touch");
+		System.out.println("------------------------------------------");
 	}
 }
