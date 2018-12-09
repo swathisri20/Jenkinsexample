f18945777e9682bbbe939d4d5f5180fd85a26be0
dckap <swathisrir@dckap.com>
2018-12-09 11:24:56 +0530

    change

--- example.java
+++ example.java
@@ -1,4 +1,4 @@
-public class helloworld{
+public class example{
 	public static void main(String[] args){
 		System.out.println("------------------------------------------");
 		System.out.println("QA touch");
